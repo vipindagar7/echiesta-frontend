88b7871db2363560230aada0124d33c6b63c665a
Vipin DAgar <vipindagar@Vipins-MacBook-Air.local>
2026-03-17 14:23:25 +0530

    change in data files

--- src/components/schedule.jsx
+++ src/components/schedule.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
+import schedule from '../data/schedule';
 
-
-const schedule = [
-  { time: "10:00 AM", event: "Opening Ceremony" },
-  { time: "11:00 AM", event: "Dance Competition" },
-  { time: "01:00 PM", event: "Battle of Bands" },
-  { time: "03:00 PM", event: "Hackathon Starts" },
-  { time: "06:00 PM", event: "Fashion Show" },
-];
 
 export default function Schedule() {
   return (
---
+++ src/data/schedule.js
@@ -0,0 +1,10 @@
+const schedule = [
+  { time: "10:00 AM", event: "Opening Ceremony" },
+  { time: "11:00 AM", event: "Dance Competition" },
+  { time: "01:00 PM", event: "Battle of Bands" },
+  { time: "03:00 PM", event: "Hackathon Starts" },
+  { time: "06:00 PM", event: "Fashion Show" },
+];
+
+
+export default schedule;
