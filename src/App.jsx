786881441f2c3bf6619c9a6ae1923e91d9dd71dd
Vipin DAgar <vipindagar@Vipins-MacBook-Air.local>
2026-03-19 18:03:29 +0530

    fix some change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/navbar';
-import VideoBackground from './components/videoBg';
-import Events from './components/event';
-import Schedule from './components/schedule';
-import Contact from './components/contact';
 import RegisterDj from './pages/Register-Dj';
 import RegisterEvent from './pages/Register-event';
 import Home from './pages/Home';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Events from '../components/event'
 import Schedule from '../components/schedule'
 import Contact from '../components/contact'
 import VideoBackground from '../components/videoBg'
-import StarBackground from '../components/starBackground'
 import RegisterDj from '../pages/Register-Dj'
 import Sponsor from '../components/sponsor'
-import EventPoster from '../components/eventPOster'
+import EventPoster from '../components/eventPoster'
 const Home = () => {
     return (
         <>
